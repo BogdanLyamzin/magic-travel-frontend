2857a0e21f4ce99a92c33c4c0c0b02b12532a789
MandorSavall <bogdan.lyamzin.d@gmail.com>
2020-11-05 21:00:31 +0200

    add homepage slider

---
+++ src/client/Home/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heading from '../../../../shared/components/Heading';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Heading>
+                Слайдер
+            </Heading>
+        </div>
+    );
+};
+
+export default HomePage;
