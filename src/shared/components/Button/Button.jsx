28e941748145b7e66cd956b1c4434b7554d7910e
MandorSavall <bogdan.lyamzin.d@gmail.com>
2020-11-08 21:42:53 +0200

    add layout

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './Button.scss';
 
-const Button = () => {
+const Button = ({children}) => {
     return (
         <button>
-
+            {children}
         </button>
     );
 };
