db6c38088235767b47e070cb16c2b4dbd456d553
Denis <deniska.vla@gmail.com>
2020-12-01 12:47:40 +0200

    add back Country add style add bootstrap add sort fix axios

--- src/app/componets/App/App.jsx
+++ src/app/componets/App/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../../shared/styles/scss/style.scss';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../../../shared/styles/scss/style.scss';
+import '../../../plugins/plugins.css';
+import '../../../plugins/style.css';
+import '../../../plugins/templete.css';
+import '../../../plugins/skin/skin-1.css';
+import '../../../plugins/slick/slick.min.css';
+import '../../../plugins/slick/slick-theme.min.css';
 import { AppRoutes } from '../../routes';
 
 export const App = () => {
