4cbee0061f9049aeffaa82925f6cef6b0dbee1f9
Denis <deniska.vla@gmail.com>
2020-12-01 17:00:51 +0200

    add back Country add style add bootstrap add sort fix axios

--- src/app/routes/AppRoutes.jsx
+++ src/app/routes/AppRoutes.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { ExamplePage } from '../../client/example/pages/ExamplePage';
+import Error404 from '../../shared/components/Error/Error';
+import ListsCountry from '../../shared/components/ItemsCountry';
+import ItemCountry from '../../shared/components/ItemCountry';
 
 export const AppRoutes = () => {
   return (
     <Switch>
       <Route exact path="/">
-        <ExamplePage />
+        {' '}
+        <Error404 />
       </Route>
+
+      <Route exact path="/ListCountry">
+        {' '}
+        <ListsCountry />
+      </Route>
+
+      <Route exact path="/ItemCountry/:id" render={() => <ItemCountry />} />
       <Route exact path="/foo">
         <p>BAR</p>
       </Route>
