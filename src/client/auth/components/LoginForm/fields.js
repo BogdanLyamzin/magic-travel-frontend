1fd45e15267c1d783ffb6439b06c803ea6866bf1
Denis <deniska.vla@gmail.com>
2020-12-09 13:19:29 +0200

    add back Country add style add bootstrap add sort fix axios

--- src/client/auth/components/LoginForm/fields.js
+++ src/client/auth/components/LoginForm/fields.js
@@ -1,21 +1,19 @@
 const loginFormFields = {
-    email: {
-        type: "email",
-        placeholder: "Email",
-        name: "email",
-    },
-    password: {
-        type: "password",
-        placeholder: "Password",
-        name: "password",
-    },
-    submit: {
-        type: "submit",
-        name: "submit",
-        value: "Sign in",
-    }
-}
-
-export {
-    loginFormFields,
+  email: {
+    type: 'email',
+    placeholder: 'Email',
+    name: 'email',
+  },
+  password: {
+    type: 'password',
+    placeholder: 'Password',
+    name: 'password',
+  },
+  submit: {
+    type: 'submit',
+    name: 'submit',
+    value: 'Sign in',
+  },
 };
+
+export { loginFormFields };
